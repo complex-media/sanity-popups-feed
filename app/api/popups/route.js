93bd827bace350d6869aad7528824d9c75d99e2b
Henry Balassiano <henry.balassiano@complex.com>
2025-07-16 02:15:24 -0400

    Fresh reset from correct directory

---
+++ app/api/popups/route.js
@@ -0,0 +1,38 @@
+export async function GET() {
+    const {
+      SANITY_PROJECT_ID,
+      SANITY_DATASET,
+      SANITY_API_VERSION,
+      SANITY_TOKEN
+    } = process.env
+  
+    const query = encodeURIComponent(`*[_type == "popUpEvent"]{
+        _id,
+        title,
+        "description": description[0].children[0].text,
+        "imageUrl": image.asset->url,
+        cta,
+        startTime,
+        endTime
+      }`)
+    const url = `https://${SANITY_PROJECT_ID}.api.sanity.io/v${SANITY_API_VERSION}/data/query/${SANITY_DATASET}?query=${query}`
+  
+    try {
+      const res = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${SANITY_TOKEN}`
+        },
+        cache: 'no-store'
+      })
+  
+      const { result } = await res.json()
+  
+      return Response.json(result)
+    } catch (err) {
+      return new Response(
+        JSON.stringify({ error: 'Sanity fetch failed', message: err.message }),
+        { status: 500, headers: { 'Content-Type': 'application/json' } }
+      )
+    }
+  }
+  
